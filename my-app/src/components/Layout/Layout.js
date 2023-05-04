b35d7897087a4ddac5302a36480a8b98cf605e14
Marlena Dulęba <110857031+marlenaduleba@users.noreply.github.com>
2023-05-04 12:13:14 +0200

    Initial commit

---
+++ my-app/src/components/Layout/Layout.js
@@ -0,0 +1,8 @@
+import css from "./Layout.module.css";
+
+export const Layout = ({children}) => {
+
+    return (
+        <main className={css.container} >{children}</main>
+    )
+}
---
+++ my-app/src/components/Task/Task.js
@@ -0,0 +1,24 @@
+import { useDispatch } from "react-redux";
+import { deleteTask, toggleCompleted } from "../../redux/operations";
+import { MdClose } from "react-icons/md";
+import css from "./Task.module.css";
+
+export const Task = ({ task }) => {
+  const dispatch = useDispatch();
+  const handleDelete = () => dispatch(deleteTask(task.id));
+  const handleToggle = () => dispatch(toggleCompleted(task));
+  return (
+    <div className={css.wrapper}>
+      <input
+        type="checkbox"
+        className={css.checkbox}
+        checked={task.completed}
+        onChange={handleToggle}
+      />
+      <p className={css.text}>{task.text}</p>
+      <button className={css.btn} type="button" onClick={handleDelete}>
+        <MdClose size={24} />
+      </button>
+    </div>
+  );
+};
